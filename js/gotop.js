f3f754f6d906093b62201226927be52d695da7cd
douwo <874952595@qq.com>
2019-01-03 00:22:29 +0800

    20190103

--- js/gotop.js
+++ js/gotop.js
@@ -6,5 +6,14 @@ $(function() {
 	});
 	$('.homepage').click(function() {
 		window.location.href = "index.html"
-	});
+	});	$(".gotop").hide(); 
+});
+
+$(window).scroll(function() {
+	// 滚动条距离顶部的距离 大于 200px时
+	if ($(window).scrollTop() >= 50) {
+		$(".gotop").fadeIn(1000); // 开始淡入
+	} else {
+		$(".gotop").stop(true, true).fadeOut(1000); // 如果小于等于 200 淡出
+	}
 });
